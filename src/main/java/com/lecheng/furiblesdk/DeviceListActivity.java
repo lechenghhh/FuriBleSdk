4c4af50ecc7ffa6451048d05e42ebe97834b01e0
lecheng <lechenggood@foxmail.com>
2018-12-29 11:27:31 +0800

    Update DeviceListActivity.java

--- src/main/java/com/lecheng/furiblesdk/DeviceListActivity.java
+++ src/main/java/com/lecheng/furiblesdk/DeviceListActivity.java
@@ -52,305 +52,108 @@ import java.util.List;
 @SuppressLint("NewApi")
 public abstract class DeviceListActivity<T> extends AppCompatActivity {
 
-    private Activity at;
-    private LeDeviceListAdapter mLeDeviceListAdapter;
+  
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
     private Handler mHandler;
-    private ProgressDialog pd;
-    private String intentPackageName = "com.lecheng.furiblesdk.BluetoothleActivity";
+    private static final long SCAN_PERIOD = 16000L;
 
-    private static final int REQUEST_ENABLE_BT = 1;
-    private static final long SCAN_PERIOD = 16000;    // 10秒后停止查找搜索.
-
-    protected abstract String setDisplayActivityPackage();//从子类传包名过来
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        getActionBar().setTitle(R.string.title_devices);
-        mHandler = new Handler();
-        // 检查当前手机是否支持ble 蓝牙,如果不支持退出程序
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
-            finish();
-        }
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
-            }
-        }
-        // 初始化 Bluetooth adapter, 通过蓝牙管理器得到一个参考蓝牙适配器(API必须在以上android4.3或以上和版本)
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-        // 检查设备上是否支持蓝牙
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-//        volleyPost();
-    }
-
-//    private void volleyPost() {
-//        if ((boolean) MySP.loadData(this, "getInfo", true)) {
-//            try {
-//                MySP.saveData(this, "getInfo", false);
-////                String url ="http://ringband.club/Furible/log.php";
-//                String url = "http://118.24.152.88/FirstJavaWeb/furiapi";
-//                StringRequest request2 = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String s) {
-//                    }
-//                }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError volleyError) {
-//                    }
-//                }) {
-//                    @Override
-//                    protected Map<String, String> getParams() throws AuthFailureError {
-//                        HashMap<String, String> map = new HashMap<String, String>();
-//                        map.put("phone", Build.BRAND + "_" + Build.MODEL);
-//                        map.put("sdk_version", Build.VERSION.RELEASE);
-//                        map.put("mac", MacUtils.getMacAddr());
-//                        map.put("login_time", MyUtils.stampToDate(System.currentTimeMillis() + ""));
-//                        return map;
-//                    }
-//                };
-//                request2.setTag("cancelPost");
-//                MyApplication.getHttpQue().add(request2);
-//            } catch (Exception e) {
-//            }
-//        }
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        if (!mScanning) {
-            menu.findItem(R.id.menu_stop).setVisible(false);
-            menu.findItem(R.id.menu_scan).setVisible(true);
-            menu.findItem(R.id.menu_refresh).setActionView(null);
-        } else {
-            menu.findItem(R.id.menu_stop).setVisible(true);
-            menu.findItem(R.id.menu_scan).setVisible(false);
-            menu.findItem(R.id.menu_refresh).setActionView(
-                    R.layout.unit_progress);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int i = item.getItemId();
-        if (i == R.id.menu_history) {//                startActivity(new Intent(this, History.class));
-        } else if (i == R.id.menu_scan) {
-            mLeDeviceListAdapter.clear();
-            scanLeDevice(true);
-        } else if (i == R.id.menu_stop) {
-            scanLeDevice(false);
-        }
-        return true;
-    }
-
-    public abstract void getLeDevices(List<BluetoothDevice> devices);
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // 为了确保设备上蓝牙能使用, 如果当前蓝牙设备没启用,弹出对话框向用户要求授予权限来启用
-        if (!mBluetoothAdapter.isEnabled()) {
-            if (!mBluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-        }
-        // Initializes list view adapter.
-        mLeDeviceListAdapter = new LeDeviceListAdapter();
-//        setListAdapter(mLeDeviceListAdapter);
-        getLeDevices(mLeDeviceListAdapter.getLeDevices());
-        scanLeDevice(true);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
-//            return;
-//        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        scanLeDevice(false);
-        mLeDeviceListAdapter.clear();
-    }
-
-//    @Override
-//    protected void onListItemClick(ListView l, View v, int position, long id) {
-//        final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
-//        if (device == null) return;
-////        final Intent intent = new Intent(this, BluetoothLeActivity.class);
-//        PackageManager packageManager = getPackageManager();
-//        System.out.println("DeviceListSelectActivity-p=" + position + "-name=" + device.getName() +
-//                "-address=" + device.getAddress() + "-package=" + setDisplayActivityPackage());
-//        Intent intent = new Intent();
-//        intent.setClassName(this, setDisplayActivityPackage());
-//        intent.putExtra(BluetoothLeActivity.EXTRAS_DEVICE_NAME, device.getName());
-//        intent.putExtra(BluetoothLeActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
-//        if (mScanning) {
-//            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-//            mScanning = false;
-//        }
-//        startActivity(intent);
-//    }
-
-    private void scanLeDevice(final boolean enable) {
-        if (enable) {
-            // Stops scanning after a pre-defined scan period.
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
-                    invalidateOptionsMenu();
-                }
-            }, SCAN_PERIOD);
-
-            mScanning = true;
-            mBluetoothAdapter.startLeScan(mLeScanCallback);
-        } else {
-            mScanning = false;
-            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-        }
-        invalidateOptionsMenu();
-    }
-
-    protected void startDisplayActivity(String deviceName, String deviceAddress,
-                                        Context context, T t) {
-        startActivity(new Intent(context, (Class<T>) t)
-                .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_NAME, deviceName)
-                .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_ADDRESS, deviceAddress));
-    }
-
-    // Adapter for holding devices found through scanning.
-    private class LeDeviceListAdapter extends BaseAdapter {
-        private ArrayList<BluetoothDevice> mLeDevices;
-        private LayoutInflater mInflator;
-
-        public LeDeviceListAdapter() {
-            super();
-            mLeDevices = new ArrayList<BluetoothDevice>();
-            mInflator = DeviceListActivity.this.getLayoutInflater();
-        }
-
-        public void addDevice(BluetoothDevice device) {
-            if (!mLeDevices.contains(device)) {
-                mLeDevices.add(device);
-            }
-        }
-
-        public List<BluetoothDevice> getLeDevices() {
-            return mLeDevices;
-        }
-
-        public BluetoothDevice getDevice(int position) {
-            return mLeDevices.get(position);
-        }
-
-        public void clear() {
-            mLeDevices.clear();
-        }
-
-        @Override
-        public int getCount() {
-            return mLeDevices.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return mLeDevices.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            ViewHolder viewHolder;
-            // General ListView optimization code.
-//            System.out.println("lechenghaha-getview");
-            if (view == null) {
-                view = mInflator.inflate(R.layout.item_device, null);
-                viewHolder = new ViewHolder();
-                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
-                view.setTag(viewHolder);
-            } else {
-                viewHolder = (ViewHolder) view.getTag();
-            }
-
-            final BluetoothDevice device = mLeDevices.get(i);
-            final String deviceName = device.getName();
-            if (deviceName != null && deviceName.length() > 0)
-                viewHolder.deviceName.setText(deviceName);
-            else viewHolder.deviceName.setText(R.string.unknown_device);
-            viewHolder.deviceAddress.setText(device.getAddress());
-
-//            startActivity(new Intent(DeviceListSelectActivity.this, BluetoothLeActivity.class)
-//                    .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_NAME, device.getName())
-//                    .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress()));
-//            if (device.getAddress().equals(MySP.loadData(DeviceListSelectActivity.this, "address", "") + "")) {
-//                new MyToast(DeviceListSelectActivity.this, "正在尝试自动连接，请稍后", 1);
-//                System.out.println("lechenghaha-getview-autoconn" + device.getAddress());
-//            pd = pd.show(DeviceListSelectActivity.this, "请稍等", "正在自动连接...");
-////                MySP.saveData(DeviceListSelectActivity.this, "address", "");
-//            new Thread() {
-//                @Override
-//                public void run() {
-//                    try {
-//                        sleep(3000);
-//                        startActivity(new Intent(DeviceListSelectActivity.this, BluetoothLeActivity.class)
-//                                .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_NAME, device.getName())
-//                                .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress()));
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                if (pd != null) pd.dismiss();
-//                                DeviceListSelectActivity.this.finish();
-//                            }
-//                        });
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }.start();
-//            }
-            return view;
-        }
-    }
-
-    // Device scan callback.
-    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
-
-        @Override
+    private LeScanCallback mLeScanCallback = new LeScanCallback() {
         public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-            runOnUiThread(new Runnable() {
-                @Override
+            DeviceListActivity.this.runOnUiThread(new Runnable() {
                 public void run() {
-                    mLeDeviceListAdapter.addDevice(device);
-                    mLeDeviceListAdapter.notifyDataSetChanged();
-                    getLeDevices(mLeDeviceListAdapter.getLeDevices());
+                    getLeDevice(device);
                 }
             });
         }
     };
 
-    static class ViewHolder {
-        TextView deviceName;
-        TextView deviceAddress;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.mHandler = new Handler();
+        if (!this.getPackageManager().hasSystemFeature("android.hardware.bluetooth_le")) {
+            Toast.makeText(this, string.ble_not_supported, Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+
+        if (VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, "android.permission.ACCESS_COARSE_LOCATION") != 0) {
+            ActivityCompat.requestPermissions(this, new String[]{"android.permission.ACCESS_COARSE_LOCATION"}, 10);
+        }
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        this.mBluetoothAdapter = bluetoothManager.getAdapter();
+        if (this.mBluetoothAdapter == null) {
+            Toast.makeText(this, string.error_bluetooth_not_supported, Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+    }
+
+    protected void scanLeDevice(boolean enable) {
+        if (enable) {
+            this.mHandler.postDelayed(new Runnable() {
+                public void run() {
+                    DeviceListActivity.this.mScanning = false;
+                    DeviceListActivity.this.mBluetoothAdapter.stopLeScan(DeviceListActivity.this.mLeScanCallback);
+                    DeviceListActivity.this.invalidateOptionsMenu();
+                    onScanStop();
+                }
+            }, SCAN_PERIOD);
+            this.mScanning = true;
+            this.mBluetoothAdapter.startLeScan(this.mLeScanCallback);
+        } else {
+            this.mScanning = false;
+            this.mBluetoothAdapter.stopLeScan(this.mLeScanCallback);
+        }
+        this.invalidateOptionsMenu();
+    }
+
+    protected abstract void onScanStop();
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        this.getMenuInflater().inflate(menu.main, menu);
+        if (!this.mScanning) {
+            menu.findItem(id.menu_stop).setVisible(false);
+            menu.findItem(id.menu_scan).setVisible(true);
+            menu.findItem(id.menu_refresh).setActionView((View) null);
+        } else {
+            menu.findItem(id.menu_stop).setVisible(true);
+            menu.findItem(id.menu_scan).setVisible(false);
+            menu.findItem(id.menu_refresh).setActionView(layout.unit_progress);
+        }
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i != id.menu_history) {
+            if (i == id.menu_scan) {
+                this.scanLeDevice(true);
+            } else if (i == id.menu_stop) {
+                this.scanLeDevice(false);
+            }
+        }
+
+        return true;
+    }
+
+    public abstract void getLeDevice(BluetoothDevice bluetoothDevice);
+
+    protected void onResume() {
+        super.onResume();
+        if (!this.mBluetoothAdapter.isEnabled() && !this.mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent("android.bluetooth.adapter.action.REQUEST_ENABLE");
+            this.startActivityForResult(enableBtIntent, 1);
+        }
+        this.scanLeDevice(true);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        this.scanLeDevice(false);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
