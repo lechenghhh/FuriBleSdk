58a12c3694d3b36675934779e84d45ef8d3f1daa
lechenggood@foxmail.com <imgh6982357>
2019-08-09 11:29:30 +0800

    增加注释 完善

--- app/src/main/java/lecheng/com/furible_demo/ContentActivity.java
+++ app/src/main/java/lecheng/com/furible_demo/ContentActivity.java
@@ -11,10 +11,15 @@ import com.lecheng.furiblesdk.BluetoothLeService;
 
 import static com.lecheng.furiblesdk.BluetoothLeActivity.EXTRAS_DEVICE_NAME;
 
+/**
+ * 用于展示数据的界面，使用时请注册manifest标签
+ * 由于需要各种权限，匹配的协议，以及bleservice的支持，因此全部封装在该父类中
+ **/
+
 public class ContentActivity extends BluetoothLeActivity {
 
-    TextView tv1, tv2;
-    String name, address;
+    private TextView tvDevice, tvGetData;
+    private String name, address;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
@@ -22,15 +27,15 @@ public class ContentActivity extends BluetoothLeActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
-        tv1 = findViewById(R.id.tvDevice);
-        tv2 = findViewById(R.id.tvContent);
-        tv1.setText(name + "-" + address);
+        tvDevice = findViewById(R.id.tvDevice);
+        tvGetData = findViewById(R.id.tvGetData);
+        tvDevice.setText(name + "-" + address);
     }
 
-    //当接受到数据后该方法会自动回调
+    /*当接受到数据后该方法会自动回调，并且只有福日电子秤才有数据 */
     @Override
     public void getData(String s, String s1, String s2, String s3) {
-        tv2.setText(s);
+        tvGetData.setText(s);
     }
 
     @Override
@@ -38,13 +43,12 @@ public class ContentActivity extends BluetoothLeActivity {
     }
 
     @Override
-    public void getConnectionInfo(String s, String s1) {
-        name = s;
-        address = s1;
+    public void getConnectionInfo(String name, String address) {
+        this.name = name;
+        this.address = address;
     }
 
     @Override
     public void getBluetoothLeService(BluetoothLeService bluetoothLeService) {
-
     }
 }
--- app/src/main/java/lecheng/com/furible_demo/ScanActivity.java
+++ app/src/main/java/lecheng/com/furible_demo/ScanActivity.java
@@ -1,34 +1,39 @@
 package lecheng.com.furible_demo;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lecheng.furiblesdk.BluetoothLeActivity;
 import com.lecheng.furiblesdk.DeviceListActivity;
 
-/*连接选择界面*/
+/*设备扫描/连接选择界面*/
 public class ScanActivity extends DeviceListActivity {
 
-    TextView tvDevice;
-    EditText etAddress;
+    LinearLayout lvDevices;
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
-        tvDevice = findViewById(R.id.tvDevice);
-        etAddress = findViewById(R.id.etAddress);
+        lvDevices = findViewById(R.id.lvDevices);
 
         //点击按钮进行设备搜索
         findViewById(R.id.btnScan).setOnClickListener(new View.OnClickListener() {
@@ -37,25 +42,49 @@ public class ScanActivity extends DeviceListActivity {
                 scanLeDevice(true);
             }
         });
+    }
 
-        //选择设备进行连接
-        findViewById(R.id.btnConn).setOnClickListener(new View.OnClickListener() {
+    /*开启蓝牙功能*/
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 初始化 Bluetooth adapter, 通过蓝牙管理器得到一个参考蓝牙适配器(API必须在以上android4.3或以上和版本)
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        // 检查设备上是否支持蓝牙
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        // 为了确保设备上蓝牙能使用, 如果当前蓝牙设备没启用,弹出对话框向用户要求授予权限来启用
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    /*当扫描到设备时，该方法会回调*/
+    @Override
+    public void getLeDevice(final BluetoothDevice bluetoothDevice) {        //如果获取到空的设备可以自行排除
+        TextView tvDevice = new TextView(this);
+        tvDevice.setPadding(10, 10, 10, 10);
+        tvDevice.setText(bluetoothDevice.getName() + " - " + bluetoothDevice.getAddress());
+        tvDevice.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {                /*跳转到要展示蓝牙数据的界面*/
                 startActivity(new Intent(ScanActivity.this, ContentActivity.class)
-                        .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_NAME, "thisDevice")
-                        .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_ADDRESS, etAddress.getText().toString().trim()));
+                        .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_NAME, bluetoothDevice.getName() + "")
+                        .putExtra(BluetoothLeActivity.EXTRAS_DEVICE_ADDRESS, bluetoothDevice.getAddress()));
             }
         });
+        lvDevices.addView(tvDevice);
     }
 
-    @Override
-    public void getLeDevice(BluetoothDevice bluetoothDevice) {
-        //如果获取到空的设备可以自行排除
-        tvDevice.append(bluetoothDevice.getName() + "-" + bluetoothDevice.getAddress() + "\n");
-    }
-
-    //菜单可能会有问题
+    /*菜单功能在某些版本可能会有异常*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         try {
