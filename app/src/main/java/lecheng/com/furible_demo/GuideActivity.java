d4cf18e136f78844538768ce6b33b771776706fa
lechenggood@foxmail.com <imgh6982357>
2019-08-13 14:52:50 +0800

    增加引导页面2222222222222

--- app/src/main/java/lecheng/com/furible_demo/GuideActivity.java
+++ app/src/main/java/lecheng/com/furible_demo/GuideActivity.java
@@ -4,19 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
-/* 此页为引导页 */
+/* 此页为引导页,跟连接电子秤无任何关系 */
 public class GuideActivity extends AppCompatActivity {
+    Button btnSkip;
+    FrameLayout flSkip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
 
-        findViewById(R.id.btnSkip).setOnClickListener(new View.OnClickListener() {
+        btnSkip = findViewById(R.id.btnSkip);
+        flSkip = findViewById(R.id.flSkip);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(GuideActivity.this, ScanActivity.class));
+                finish();
+            }
+        });
+        flSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flSkip.setVisibility(View.GONE);
+            }
+        });
+        findViewById(R.id.vGuide).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlphaAnimation aa = new AlphaAnimation(0f, 1.0f);
+                aa.setDuration(1000);
+                aa.setFillAfter(true);
+                flSkip.startAnimation(aa);
+                flSkip.setVisibility(View.VISIBLE);
             }
         });
     }
